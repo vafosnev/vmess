a601995d4444ed6e75e685b5856968ccec360c4a
刘栋19950828 <94947393+smallflowercat1995@users.noreply.github.com>
2022-10-29 14:08:37 +0800

    Update getorupdatefile.sh

--- getorupdatefile.sh
+++ getorupdatefile.sh
@@ -80,8 +80,8 @@ getStartNgrok(){
     if [[ -z "$HAS_ERRORS" ]]; then
       echo ""
       echo "=========================================="
-      echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-      echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
+      echo "To connect: $(grep -o -E "tcp://(.+)" < ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+      echo "or conenct with $(grep -o -E "tcp://(.+)" < ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
       echo "=========================================="
     else
       echo "$HAS_ERRORS"
