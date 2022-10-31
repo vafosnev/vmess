615cf29367a939cc257101e0527044cd80d64c2d
刘栋19950828 <94947393+smallflowercat1995@users.noreply.github.com>
2022-10-31 16:05:46 +0800

    Update getorupdatefile.sh

--- getorupdatefile.sh
+++ getorupdatefile.sh
@@ -112,7 +112,7 @@ getStartV2ray(){
     unzip -o ${FILE_NAME} -d $(echo $FILE_NAME | sed 's;.zip;;g') ; cd $(echo $FILE_NAME | sed 's;.zip;;g')
 
     # 解除环境变量
-    rm -rfv  ${FILE_NAME}
+    rm -rfv  ../${FILE_NAME}
 
     # 解除环境变量
     unset DOWNLOAD URI_DOWNLOAD FILE_NAME
@@ -151,30 +151,8 @@ cat << EOF >> config.json
 		}
 	],
 	"routing": {
-		"strategy": "rules",
-		"settings": {
-			"rules": [{
-				"type": "field",
-				"ip": [
-					"0.0.0.0/8",
-					"10.0.0.0/8",
-					"100.64.0.0/10",
-					"127.0.0.0/8",
-					"169.254.0.0/16",
-					"172.16.0.0/12",
-					"192.0.0.0/24",
-					"192.0.2.0/24",
-					"192.168.0.0/16",
-					"198.18.0.0/15",
-					"198.51.100.0/24",
-					"203.0.113.0/24",
-					"::1/128",
-					"fc00::/7",
-					"fe80::/10"
-				],
-				"outboundTag": "blocked"
-			}]
-		}
+	"domainStrategy": "IPIfNonMatch",
+	"rules": []
 	}
 }
 EOF
